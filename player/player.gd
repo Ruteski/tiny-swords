e5e89e576415d42754f47b879696fc00310a09a2
Lincoln Ruteski <lruteski@gmail.com>
2024-05-01 16:55:24 -0300

    programado o ataque do player

--- player/player.gd
+++ player/player.gd
@@ -3,10 +3,13 @@ extends CharacterBody2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var sprite: Sprite2D = $Sprite2D
 
-@export var is_running: bool = false
 @export var speed: int = 3 # ou coloco 3 e multiplico la embaixo por 100?
 @export var speed_factor: float = 100.0
 @export_range(0,1) var lerp_factor: float = 0.2
+
+var is_running: bool = false
+var is_attacking: bool = false
+var attack_cooldown: float = 0.0
 
 # update da unity
 #func _process(delta: float) -> void:
@@ -18,9 +21,22 @@ extends CharacterBody2D
 			#animation_player.play("run")
 			#is_running = true
 
+# TODO: adicionar ataques pra cima pra baixo direita e esquerda
+
+func _process(delta: float) -> void:
+	if is_attacking:
+		attack_cooldown -= delta
+		if attack_cooldown <= 0.0:
+			is_attacking = false
+			is_running = false
+			animation_player.play("idle")
+
 # fixed_update da unity
 func _physics_process(delta: float) -> void:
 	move()
+	
+	if Input.is_action_just_pressed("attack"):
+		attack()
 
 
 func move() -> void:
@@ -34,6 +50,9 @@ func move() -> void:
 		input_vector.y = 0.0
 	
 	var target_velocity = input_vector * speed * speed_factor
+	if is_attacking:
+		target_velocity *= .25
+	
 	velocity = lerp(velocity, target_velocity, lerp_factor)
 	move_and_slide()
 	invert_side(input_vector)
@@ -44,11 +63,12 @@ func play_animation(input_vector: Vector2) -> void:
 	var was_running: bool = is_running
 	is_running = not input_vector.is_zero_approx()
 	
-	if was_running != is_running:
-		if is_running:
-			animation_player.play("run")
-		else:
-			animation_player.play("idle")
+	if not is_attacking:
+		if was_running != is_running:
+			if is_running:
+				animation_player.play("run")
+			else:
+				animation_player.play("idle")
 
 
 func invert_side(input_vector: Vector2) -> void:
@@ -57,3 +77,12 @@ func invert_side(input_vector: Vector2) -> void:
 	elif input_vector.x < 0:
 		sprite.flip_h = true
 
+
+func attack() -> void:
+	if is_attacking:
+		return
+
+	animation_player.play("attack_side_right_down")
+	attack_cooldown = 0.6
+	is_attacking = true
+	
