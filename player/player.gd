49aadd1290d80ad117a6bf0bd7c5df9ae746b2cd
Lincoln Ruteski <lruteski@gmail.com>
2024-05-01 17:19:37 -0300

    melhorado script do player

--- player/player.gd
+++ player/player.gd
@@ -7,7 +7,9 @@ extends CharacterBody2D
 @export var speed_factor: float = 100.0
 @export_range(0,1) var lerp_factor: float = 0.2
 
+var input_vector: Vector2 = Vector2.ZERO
 var is_running: bool = false
+var was_running: bool = false
 var is_attacking: bool = false
 var attack_cooldown: float = 0.0
 
@@ -23,46 +25,56 @@ var attack_cooldown: float = 0.0
 
 # TODO: adicionar ataques pra cima pra baixo direita e esquerda
 
+
+# update da unity
 func _process(delta: float) -> void:
+	read_input()
+	
+	#update_attack_cooldown(delta)
+	if Input.is_action_just_pressed("attack"):
+		attack()
+	
+	rotate_sprite(input_vector)
+	play_animation(input_vector)
+
+
+func update_attack_cooldown(delta: float) -> void:
 	if is_attacking:
 		attack_cooldown -= delta
 		if attack_cooldown <= 0.0:
 			is_attacking = false
 			is_running = false
-			animation_player.play("idle")
+			animation_player.play("idle")	
 
 # fixed_update da unity
 func _physics_process(delta: float) -> void:
 	move()
-	
-	if Input.is_action_just_pressed("attack"):
-		attack()
 
 
 func move() -> void:
-	var input_vector: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
-	
-	# apaga deadzone do input vector(principalmente quando o controle esta conectado)
-	var deadzone: float = 0.15
-	if abs(input_vector.x) < deadzone:
-		input_vector.x = 0.0
-	if abs(input_vector.y) < deadzone:
-		input_vector.y = 0.0
-	
 	var target_velocity = input_vector * speed * speed_factor
 	if is_attacking:
 		target_velocity *= .25
 	
 	velocity = lerp(velocity, target_velocity, lerp_factor)
 	move_and_slide()
-	invert_side(input_vector)
-	play_animation(input_vector)
+
+
+func read_input() -> void:
+	input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var deadzone: float = 0.15
+	
+	# apaga deadzone do input vector(principalmente quando o controle esta conectado)
+	if abs(input_vector.x) < deadzone:
+		input_vector.x = 0.0
+	if abs(input_vector.y) < deadzone:
+		input_vector.y = 0.0
+		
+	was_running = is_running
+	is_running = not input_vector.is_zero_approx()
 
 
 func play_animation(input_vector: Vector2) -> void:
-	var was_running: bool = is_running
-	is_running = not input_vector.is_zero_approx()
-	
 	if not is_attacking:
 		if was_running != is_running:
 			if is_running:
@@ -71,7 +83,7 @@ func play_animation(input_vector: Vector2) -> void:
 				animation_player.play("idle")
 
 
-func invert_side(input_vector: Vector2) -> void:
+func rotate_sprite(input_vector: Vector2) -> void:
 	if input_vector.x > 0:
 		sprite.flip_h = false
 	elif input_vector.x < 0:
@@ -85,4 +97,5 @@ func attack() -> void:
 	animation_player.play("attack_side_right_down")
 	attack_cooldown = 0.6
 	is_attacking = true
-	
+
+
