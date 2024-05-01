15df2cefdabcea182e52e1cd7f63c72a3d94cbe6
Lincoln Ruteski <lruteski@gmail.com>
2024-05-01 20:45:05 -0300

    iniciado o script do inimigo, criado a classe Enemy

--- enemies/behaviors/follow_player.gd
+++ enemies/behaviors/follow_player.gd
@@ -1,12 +1,17 @@
-extends CharacterBody2D
+extends Node
 
-
-@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 @export var speed: float = 1.0
 
+
+var enemy: Enemy
+var sprite: AnimatedSprite2D
 var input_vector: Vector2 = Vector2.ZERO
 
+
+func _ready():
+	enemy = get_parent()
+	sprite = enemy.get_node("AnimatedSprite2D") # aqui eu passo o nome do Node
 
 func _process(delta: float) -> void:
 	rotate_sprite()
@@ -14,11 +19,11 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	var player_position = GameManager.player_position
-	var diference = player_position - position
+	var diference = player_position - enemy.position
 	input_vector = diference.normalized()
 	
-	velocity = input_vector * speed * 100.0
-	move_and_slide()
+	enemy.velocity = input_vector * speed * 100.0
+	enemy.move_and_slide()
  
 
 func rotate_sprite() -> void:
---
+++ enemies/enemy.gd
@@ -0,0 +1,6 @@
+class_name Enemy
+
+extends Node2D
+
+
+@export var health: int = 10
