0df55412d67f417f7fa06fb1c040b56729a7e188
Lincoln Ruteski <lruteski@gmail.com>
2024-05-05 20:21:27 -0300

    drops aleatorio dos mobs

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -2,12 +2,18 @@ class_name Enemy
 
 extends Node2D
 
-
+@export_group("Life")
 @export var health: int = 10
 @export var death_prefab: PackedScene
 @export var deal_damage_amount: int = 2
 
+@export_group("Drops")
+@export var drop_chance: float = 0.1
+@export var drop_items: Array[PackedScene]
+@export var drop_chances: Array[float]
+
 @onready var damage_digit_marker = $DamageDigitMarker
+
 
 var damage_ditig_prefab: PackedScene
 
@@ -41,11 +47,46 @@ func damage(amount: int) -> void:
 		
 		
 func die() -> void:
+	# cria a caveira
 	if death_prefab:
 		var death_object = death_prefab.instantiate()
 		death_object.position = position
 		get_parent().add_child(death_object)
 		
+	# cria o drop
+	if randf() <= drop_chance:
+		drop_item()
+		
 	queue_free()
 
+
+func drop_item() -> void:
+	var drop = get_random_drop_item().instantiate()
+	drop.position = position
+	get_parent().add_child(drop)
+
+
+func get_random_drop_item() -> PackedScene:
+	if drop_items.size() == 1:
+		return drop_items[0]
+	
+	# calcular chance maxima
+	var max_chance: float = 0
+	for drop_chance in drop_chances:
+		max_chance += drop_chance
+		
+	# jogar o dado
+	var random_value = randf() * max_chance
+	
+	# girar a roleta
+	var needle: float = 0
+	for i in drop_items.size():
+		var drop_item = drop_items[i]
+		var drop_chance = drop_chances[i] if i < drop_chances.size() else 1
+		if random_value <= drop_chance + needle:
+			return drop_item
+			
+	return drop_items[0]
+		
+	
 
